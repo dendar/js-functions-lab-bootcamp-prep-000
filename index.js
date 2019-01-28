5ecf6022598f339d671a70ece2f9e39a95073a89
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-28 04:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days){
-return   `lt's ${days} days until ${holiday}!`
+return   `lt\'s ${days} days until ${holiday}!`
 
 }
