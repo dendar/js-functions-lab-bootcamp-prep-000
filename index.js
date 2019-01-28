008f7d5a9a4130f21130a9377564e645e7182118
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-28 04:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays"
+}
+function happyHolidaysTo(name){
+  `Happy holidays, ${name}!`
+}
+function happyHolidayTo(holiday, name){
+  `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday){
+  `It's ${days} days until ${holiday}!`
+}
