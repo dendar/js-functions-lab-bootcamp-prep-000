d41a20f23b1bd264916e1961333bf456b65715b0
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-28 04:27:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-// function happyHolidaysTo(name){
-//   `Happy holidays, ${name}!`
-// }
+ function happyHolidaysTo(name){
+  `Happy holidays, ${name}!`
+ }
 // function happyHolidayTo(holiday, name){
 //   `Happy ${holiday}, ${name}!`
 // }
