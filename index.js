39a7addb1f3088a859131b61ed26ea644bd47c3b
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-28 04:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function happyHolidays(){
   return "Happy holidays"
 }
-function happyHolidaysTo(name){
-  `Happy holidays, ${name}!`
-}
-function happyHolidayTo(holiday, name){
-  `Happy ${holiday}, ${name}!`
-}
-function holidayCountdown(days, holiday){
-  `It's ${days} days until ${holiday}!`
-}
+// function happyHolidaysTo(name){
+//   `Happy holidays, ${name}!`
+// }
+// function happyHolidayTo(holiday, name){
+//   `Happy ${holiday}, ${name}!`
+// }
+// function holidayCountdown(days, holiday){
+//   `It's ${days} days until ${holiday}!`
+// }
